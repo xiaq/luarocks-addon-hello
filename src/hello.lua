e7911e4d18350bf8389d4ebc612dcef9edd06f9b
Cheer Xiao <xiaqqaix@gmail.com>
2015-06-21 14:40:21 +0200

    F1R57 C0MM17!

---
+++ src/hello.lua
@@ -0,0 +1,19 @@
+local hello = {}
+
+function hello.load()
+    print "Hello!"
+    print "You have loaded the addon 'hello' in your rockspec."
+    print "I do nothing yet, however."
+end
+
+function hello.run(...)
+    print "Hello!"
+    print "You have run the command 'hello' provided by the eponymous addon."
+    print "The arguments you gave to me are:"
+    for i, a in ipairs({...}) do
+        print(i, a)
+    end
+    print "I do nothing yet, however."
+end
+
+return hello
